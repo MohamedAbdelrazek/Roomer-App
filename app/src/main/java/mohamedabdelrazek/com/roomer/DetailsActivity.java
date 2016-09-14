76a35cae34a5d6f3921e027059349c28e061c627
MuhammedAbdelraZek <muhamedrabi3@gmail.com>
2016-09-14 23:17:54 +0200

    StableVersion

--- app/src/main/java/mohamedabdelrazek/com/roomer/DetailsActivity.java
+++ app/src/main/java/mohamedabdelrazek/com/roomer/DetailsActivity.java
@@ -6,28 +6,44 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 import mohamedabdelrazek.com.roomer.GuestsData.GuestModel;
+import mohamedabdelrazek.com.roomer.GuestsData.GuestsContract;
 import mohamedabdelrazek.com.roomer.GuestsData.ManageDataBase;
 
 public class DetailsActivity extends AppCompatActivity {
     private GuestModel guestModel;
     private ManageDataBase zManageDataBase;
-
+    private TextView dNAme;
+    private TextView dAge;
+    private TextView dID;
+    private ImageView dImageView;
+    private TextView dGender;
+    private TextView dAddress;
+    private TextView dMobile;
+    private TextView dEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         Intent intent = getIntent();
 
-        zManageDataBase=new ManageDataBase(this);
+        zManageDataBase = new ManageDataBase(this);
         guestModel = (GuestModel) intent.getSerializableExtra("data");
-        TextView textView = (TextView) findViewById(R.id.dName);
-        textView.setText(guestModel.getName());
+        dNAme = (TextView) findViewById(R.id.dName);
+        dAddress = (TextView) findViewById(R.id.dAddress);
+        dAge = (TextView) findViewById(R.id.dAge);
+        dID = (TextView) findViewById(R.id.dId);
+        dGender = (TextView) findViewById(R.id.dGender);
+        dEmail = (TextView) findViewById(R.id.dEmail);
+        dMobile = (TextView) findViewById(R.id.dMobile_number);
+        dImageView= (ImageView) findViewById(R.id.dImgView);
+        setActivityValues();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,7 +59,7 @@ public class DetailsActivity extends AppCompatActivity {
                 intent.putExtra("UPDATE", guestModel);
                 startActivity(intent);
                 return true;
-            case  R.id.action_remove:
+            case R.id.action_remove:
                 removeGuestInformation(guestModel.getId());
                 return true;
 
@@ -52,9 +68,8 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     private void removeGuestInformation(int id) {
-       int result=  zManageDataBase.removeGustInformation(id);
-        if (result!=0)
-        {
+        int result = zManageDataBase.removeGustInformation(id);
+        if (result != 0) {
             final ProgressDialog progress = ProgressDialog.show(DetailsActivity.this, "removing guest Info ...",
                     "please wait", true);
 
@@ -84,5 +99,29 @@ public class DetailsActivity extends AppCompatActivity {
         }
 
     }
+
+    private void setActivityValues() {
+
+        dNAme.setText(guestModel.getName());
+        dAge.setText(""+guestModel.getAge());
+        dID.setText(""+guestModel.getId());
+        dAddress.setText(guestModel.getAddress());
+        dMobile.setText(guestModel.getPhone());
+        dEmail.setText(guestModel.getE_mail());
+        int gender= guestModel.getGender();
+        if (gender== GuestsContract.GuestsEntry.GENDER_MALE)
+        {
+            dGender.setText("Male");
+            dImageView.setImageResource(R.drawable.male_main_img);
+
+        }
+        else
+        {
+            dGender.setText("Female");
+            dImageView.setImageResource(R.drawable.female_main_img);
+        }
+
+    }
+
 
 }
